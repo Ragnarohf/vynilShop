4b3df0ff941a5a6c22787cfc88cc2ccf55c20a11
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-08 11:55:44 +0200

    message

--- index.php
+++ index.php
@@ -2,29 +2,38 @@
 require_once("./inc/functions.php");
 include('./inc/header.php');
 $vinyles = selectAllVinyles("title");
-var_dump($vinyles);
-die();
+// var_dump($vinyles);
+// die();
 
 ?>
 
 <div id="slider"></div>
-
-<section id='article'>
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="coverImg" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">title </h5>
-            <p class="card-text">artiste</p>
-            <p class="card-text">genre</p>
-
-            <a href="#" class="btn btn-primary">Plus d'infos...</a>
-        </div>
-    </div>
-</section>
-
-
-
 <?php
-include('./inc/footer.php');
+for ($i = 0; $i < count($vinyles); $i++) {
 ?>
-<script src="./assets/js/carouselMultiEffets.js"></script>
+
+
+
+    <section id='article'>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="coverImg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">title </h5>
+                <p class="card-text">artiste</p>
+                <p class="card-text">genre</p>
+
+                <a href="#" class="btn btn-primary">Plus d'infos...</a>
+            </div>
+        </div>
+    <?php
+}
+    ?>
+
+    </section>
+
+
+
+    <?php
+    include('./inc/footer.php');
+    ?>
+    <script src="./assets/js/carouselMultiEffets.js"></script>
