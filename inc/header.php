15a059413c54ddc42a614eb2ffad35c059770cfa
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-08 10:00:53 +0200

    message

--- inc/header.php
+++ inc/header.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>VinylShop</title>
+
     <script src="./assets/js/carouselMultiEffets.js"></script>
-
-
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>VinylShop</title>
 </head>
 
 <body>
