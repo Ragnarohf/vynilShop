d823e830c9871a9e82f8766fe0cd1036d924194e
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-08 12:07:52 +0200

    message

--- index.php
+++ index.php
@@ -11,16 +11,13 @@ $vinyles = selectAllVinyles("title");
 <?php
 for ($i = 0; $i < count($vinyles); $i++) {
 ?>
-
-
-
     <section id='article'>
         <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="coverImg" alt="Card image cap">
+            <img class="card-img-top" src="./assets/cover/<?php echo $vinyles[$i]['cover_img'] ?>" alt="<?php echo $vinyles[$i]['title'] . " - " . $vinyles[$i]['artiste'] ?>">
             <div class="card-body">
-                <h5 class="card-title">title </h5>
-                <p class="card-text">artiste</p>
-                <p class="card-text">genre</p>
+                <h5 class="card-title"><?php echo $vinyles[$i]['title'] ?></h5>
+                <p class="card-text"><?php echo $vinyles[$i]['artiste'] ?></p>
+                <p class="card-text"><?php echo $vinyles[$i]['genre'] ?></p>
 
                 <a href="#" class="btn btn-primary">Plus d'infos...</a>
             </div>
