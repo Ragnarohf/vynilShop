51d3f0a065f02cc5d8ed3185708333c8a92e3f9a
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-14 15:24:00 +0200

    bdd

--- vinyl_shop.sql
+++ vinyl_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 14 avr. 2021 à 13:21
+-- Généré le : mer. 14 avr. 2021 à 13:22
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `vinyl_shop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `login` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pwd` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `addr1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `addr2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cp` int(6) NOT NULL,
+  `ville` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Tel` int(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nom`, `prenom`, `login`, `pwd`, `role`, `email`, `addr1`, `addr2`, `cp`, `ville`, `Tel`) VALUES
+(10, 'Sandra', 'Nicouette', 'azerty', '$argon2i$v=19$m=65536,t=4,p=1$ZGcuS3VQUkx3dlRMdGNoNQ$W/UrPoPsTjjsLTvl+xj352k8FRfVX0jTFLlHAAatbc4', 'user', 'sandranicouette@dodo.com', '0', 'escalier de l\'ascenseur', 76200, 'panambeauGosse', 600000000),
+(11, 'admin', 'admin', 'admin', '$argon2i$v=19$m=65536,t=4,p=1$ZGcuS3VQUkx3dlRMdGNoNQ$W/UrPoPsTjjsLTvl+xj352k8FRfVX0jTFLlHAAatbc4', 'role_admin', 'admin@admin.fr', '0', 'escalier admin', 76200, 'dieppe', 200000000);
 
 -- --------------------------------------------------------
 
