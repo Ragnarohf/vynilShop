785cac90a800ce441fa9854e9631efad3f7b9387
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-16 13:57:50 +0200

    rectif js cote vue

--- assets/js/formulaireSend.js
+++ assets/js/formulaireSend.js
@@ -12,8 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(function (response) {
         response.json().then(function (result) {
           for (let indice in result) {
-            document.getElementById("success").innerHTML +=
-              result[indice] + "<br>";
+            if (document.querySelector("#" + indice + "Msg")) {
+              document.querySelector("#" + indice + "Msg").remove();
+              document.querySelector("#" + indice).style.border =
+                "1px solid black";
+            }
+            // conditions pour evitez que a div se repete
+            let divErreur = document.createElement("div");
+            divErreur.classList.add("error");
+            divErreur.id = indice + "Msg";
+            divErreur.innerText = result[indice];
+            document.querySelector("#" + indice).after(divErreur);
+            document.querySelector("#" + indice).style.border = "1px solid red";
           }
         });
       })
--- formulaire.php
+++ formulaire.php
@@ -30,41 +30,14 @@ if (!empty($_GET)) {
         <form class="formulaire" action="./validator.php" method="POST" enctype="multipart/form-data" name="uploadMP3">
             <label for="mp3">MP3</label>
             <input type="file" name="mp3" id="mp3">
-            <?php // detectt if $erreur['mp3'] est vide ou pas
-            //sinon afficher cette erreur en rouge
-            if (array_key_exists('mp3', $erreur)) {
-                echo "<div class='error'>" . $erreur['mp3'] . "</div>";
-            }
-            ?>
+
             <label for="coverImg">Cover</label>
             <input type="file" name="coverImg" id="coverImg">
-            <?php // detectt if $erreur['mp3'] est vide ou pas
-            //sinon afficher cette erreur en rouge
-            if (array_key_exists('coverImg', $erreur)) {
-                echo "<div class='error'>" . $erreur['coverImg'] . "</div>";
-            }
-            ?>
+
             <input type="text" name="title" placeholder="Titre" id="title">
-            <?php // detectt if $erreur['mp3'] est vide ou pas
-            //sinon afficher cette erreur en rouge
-            if (array_key_exists('title', $erreur)) {
-                echo "<div class='error'>" . $erreur['title'] . "</div>";
-            }
-            ?>
             <input type="text" name="artiste" placeholder="Artiste" id="artiste">
-            <?php // detectt if $erreur['mp3'] est vide ou pas
-            //sinon afficher cette erreur en rouge
-            if (array_key_exists('artiste', $erreur)) {
-                echo "<div class='error'>" . $erreur['artiste'] . "</div>";
-            }
-            ?>
             <input type="text" name="genre" placeholder="Genre" id="genre">
-            <?php // detectt if $erreur['mp3'] est vide ou pas
-            //sinon afficher cette erreur en rouge
-            if (array_key_exists('genre', $erreur)) {
-                echo "<div class='error'>" . $erreur['genre'] . "</div>";
-            }
-            ?>
+
             <select name="annee" id="annee">
                 <option value="" disabled selected>Choisissez l'année</option>
                 <?php
