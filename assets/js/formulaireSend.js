5b5c5c3a9d538f3e070e99fa37b7693af727657c
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-16 09:51:55 +0200

    ajax form

--- assets/js/formulaireSend.js
+++ assets/js/formulaireSend.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.forms["uploadMP3"];
+  document.forms["uploadMP3"].addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+  });
 });
